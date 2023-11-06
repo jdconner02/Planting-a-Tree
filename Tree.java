750dfa42c376509e07010a5bba07003235adf1a3
jdconner02 <jdconner02@gmail.com>
2023-11-05 20:23:59 -0500

    Adding my name and the intent to make a tree
    Added my name

--- Tree.java
+++ Tree.java
@@ -4,6 +4,7 @@ public class Tree{
   public static void main(String[] args){
     // This method will introduce myself and print a tree
     System.out.println("Hey there, I'm Arturo and I'm learning to code in Java");
+    System.out.println("Hi I am Jake and I will be adding a tree as well");
     System.out.println("I'm going to plant a tree today!");
     System.out.println("    *  ");
     System.out.println("   *** ");
@@ -14,3 +15,4 @@ public class Tree{
 
   }
 }
+
